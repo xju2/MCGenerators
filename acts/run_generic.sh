a86d978d62581d204c4abb6415794b88f40c831c
xju <xju@cern.ch>
2021-06-28 15:48:16 -0700

    add rivet and delphes

--- acts/run_generic.sh
+++ acts/run_generic.sh
@@ -5,7 +5,8 @@ if [ $# -lt 3 ]; then
 	exit
 fi
 
-if [ ! -f itk-pixel-digitization.json ];then
+DIGICONF=default-smearing-config-generic.json
+if [ ! -f $DIGICONF ];then
 	echo "digitization configuration, itk-pixel-digitization.json, does not exist"
 	exit
 fi
@@ -13,6 +14,12 @@ fi
 ACTSBINDIR=$1 # acts bin directory
 OUTDIR=$2	  # output directory
 PY8CMD=$3
+SKIPEVTS=2500
+
+if [ ! -d $OUTDIR ];then
+	mkdir -p $OUTDIR
+fi
+
 
 NEVTS=5000
 NPU=0
@@ -33,22 +40,33 @@ do
 	PY8OPTIONS="$PY8OPTIONS --gen-hard-process \"$ARG\""
 done < $PY8CMD
 
-
+#echo $PY8OPTIONS
 # generate events
-./ActsExamplePythia8 --gen-npileup $NPU --gen-cms-energy-gev $CMS \
-	--gen-vertex-xy-std-mm 2 --gen-vertex-z-std-mm 150 \
-	$PY8OPTIONS --output-dir $OUTDIR --output-csv -j $NWORKERS -n $NEVTS
+function gen() {
+	$ACTSBINDIR/ActsExamplePythia8 --gen-npileup $NPU --gen-cms-energy-gev $CMS \
+		--gen-vertex-xy-std-mm 2 --gen-vertex-z-std-mm 150 \
+		"$PY8OPTIONS" --output-dir $OUTDIR --output-csv -j $NWORKERS -n $NEVTS
+}
 
 
 # simulation
-./ActsExampleFatrasGeneric --input-dir $OUTDIR \
-	--output-dir $OUTDIR $BFIELD --output-csv -j $NWORKERS
+function sim() {
+	$ACTSBINDIR/ActsExampleFatrasGeneric --input-dir $OUTDIR \
+		--output-dir $OUTDIR $BFIELD --output-csv -j $NWORKERS
+}
 
 # digitization
-./ActsExampleDigitizationGeneric $BFIELD --output-dir $OUTDIR \
-	--output-csv --input-dir $OUTDIR \
-	--digi-config-file itk-pixel-digitization.json -j $NWORKERS
+function digi() {
+	$ACTSBINDIR/ActsExampleDigitizationGeneric $BFIELD --output-dir $OUTDIR \
+		--output-csv --input-dir $OUTDIR \
+		--digi-config-file $DIGICONF -j $NWORKERS
+}
 
 # measurements to spacepoints
-./ActsExampleMeasurements2SPGeneric $BFIELD --output-dir $OUTDIR \
-	--output-csv --input-dir $OUTDIR -j $NWORKERS
+function meas2sp() {
+	$ACTSBINDIR/ActsExampleMeasurements2SPGeneric $BFIELD --output-dir $OUTDIR \
+		--output-csv --input-dir $OUTDIR -j $NWORKERS
+}
+
+digi
+meas2sp
