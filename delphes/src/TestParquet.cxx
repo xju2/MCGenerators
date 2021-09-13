0649d68513576e949cd5c1d414091071eecbd8df
Xiangyang Ju <xiangyang.ju@gmail.com>
2021-09-13 14:08:24 -0700

    remove all Parquet

--- delphes/src/TestParquet.cxx
+++
@@ -1,9 +0,0 @@
-#include "DelphesParquet/DelphesParquet.hpp"
-#include <string>
-
-int main(int argc, char** argv)
-{
-    std::string outname = "dummy.parquet";
-    DelphesParquet parquet(outname);
-    return 0;
-}
