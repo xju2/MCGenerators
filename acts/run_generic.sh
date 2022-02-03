9a0d61d48a32652822df8c07c57bfc4d8bb9d2f2
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-02-03 14:09:10 -0800

    with a py8 script

--- acts/run_generic.sh
+++ acts/run_generic.sh
@@ -12,8 +12,8 @@ if [ $# -lt 7 ]; then
 	exit
 fi
 
-DIGICONF=default-smearing-config-generic.json
-#DIGICONF=default-geometric-config-generic.json
+#DIGICONF=default-smearing-config-generic.json
+DIGICONF=default-geometric-config-generic.json
 
 #DIGICONF=default-input-config-generic.json ## do not use this
 
@@ -30,7 +30,9 @@ NEVTS=$5
 NPU=$6
 SEED=$7
 
-SHIFTER="shifter --image=docexoty/heptools:ubuntu20.04"
+#SHIFTER="shifter --image=docexoty/heptools:ubuntu20.04"
+#SHIFTER="docker run --rm -v $PWD:$PWD -w $PWD docexoty/exatrkx:tf2.5-torch1.9-cuda11.2-ubuntu20.04-rapids21.10-devel-hep bash "
+SHIFTER=""
 
 if [ ! -d $OUTDIR ];then
 	mkdir -p $OUTDIR
@@ -40,9 +42,9 @@ CMS=13000
 NWORKERS=1
 BFIELD="--bf-constant-tesla 0:0:2"
 
+PY8OPTIONS=""
 function get_py8_cmd() {
 	# pythia configuration
-	PY8OPTIONS=""
 	while IFS= read -r line
 	do
 		if [ -z "$line" ] || [ ${line:0:1} == "#" ]; then
@@ -61,17 +63,19 @@ function get_py8_cmd() {
 # xy-std, 0.0125 mm
 # z-std,  55.5 mm
 function gen() {
+	get_py8_cmd 
+
 	$SHIFTER $ACTSBINDIR/ActsExamplePythia8 --gen-npileup $NPU --gen-cms-energy-gev $CMS \
 		--gen-nhard 1 --rnd-seed $SEED  --gen-pdg-beam0 2212 --gen-pdg-beam1 2212 \
-	    --output-dir $OUTDIR --output-csv -j $NWORKERS -n $NEVTS \
-		--gen-hard-process "Print:quiet = off" --gen-hard-process "50:new = N2 N2 2 0 0 15.0 0.0 0.0 0.0 100.0 0 1 0 1 0" --gen-hard-process "50:isResonance = false" --gen-hard-process "50:addChannel = 1 0.50 23 -13 13 -14" --gen-hard-process "50:addChannel = 1 0.50 23 13 -13 14" --gen-hard-process "50:mayDecay = on" --gen-hard-process "WeakSingleBoson:ffbar2W = on" --gen-hard-process "24:onMode = off" --gen-hard-process "24:addchannel = 1 1.0 103 -13 50" --gen-hard-process "ParticleDecays:limitTau0 = off" --gen-hard-process "ParticleDecays:tau0Max = 600.0" --gen-hard-process "Tune:ee = 7" --gen-hard-process "PDF:pSet = 13" --gen-hard-process "ColourReconnection:range = 1.71" --gen-hard-process "StandardModel:sin2thetaW = 0.23113" --gen-hard-process "StandardModel:sin2thetaWbar = 0.23146" --gen-hard-process "SpaceShower:pTmaxMatch = 1" --gen-hard-process "SpaceShower:pTmaxFudge = 1" --gen-hard-process "SpaceShower:MEcorrections = off" --gen-hard-process "TimeShower:pTmaxMatch = 1" --gen-hard-process "TimeShower:pTmaxFudge = 1" --gen-hard-process "TimeShower:MEcorrections = off" --gen-hard-process "TimeShower:globalRecoil = on" --gen-hard-process "TimeShower:limitPTmaxGlobal = on" --gen-hard-process "TimeShower:nMaxGlobalRecoil = 1" --gen-hard-process "TimeShower:globalRecoilMode = 2" --gen-hard-process "TimeShower:nMaxGlobalBranch = 1." --gen-hard-process "TimeShower:weightGluonToQuark=1." --gen-hard-process "Check:epTolErr = 1e-2" --gen-hard-process "SpaceShower:rapidityOrder = on" --gen-hard-process "SigmaProcess:alphaSvalue = 0.140" --gen-hard-process "SpaceShower:pT0Ref = 1.56" --gen-hard-process "SpaceShower:pTdampFudge = 1.05" --gen-hard-process "SpaceShower:alphaSvalue = 0.127" --gen-hard-process "TimeShower:alphaSvalue = 0.127" --gen-hard-process "BeamRemnants:primordialKThard = 1.88" --gen-hard-process "MultipartonInteractions:pT0Ref = 2.09" --gen-hard-process "MultipartonInteractions:alphaSvalue = 0.126"
+		--output-dir "$OUTDIR" --output-csv -j $NWORKERS -n $NEVTS \
+		"$PY8OPTIONS"
 }
 
 
 # simulation
 function sim() {
-	$SHIFTER $ACTSBINDIR/ActsExampleFatrasGeneric --input-dir $OUTDIR \
-		--output-dir $OUTDIR $BFIELD --output-csv -j $NWORKERS
+	$SHIFTER $ACTSBINDIR/ActsExampleFatrasGeneric --input-dir "$OUTDIR" \
+		--output-dir "$OUTDIR" $BFIELD --output-csv -j $NWORKERS
 }
 
 # digitization
@@ -83,10 +87,8 @@ function digi() {
 
 # measurements to spacepoints
 function meas2sp() {
-	$SHIFTER $ACTSBINDIR/ActsExampleMeasurements2SPGeneric $BFIELD --output-dir $OUTDIR \
+	$SHIFTER $ACTSBINDIR/ActsExampleMeasurementsToSPGeneric $BFIELD --output-dir $OUTDIR \
 		--output-csv --input-dir $OUTDIR -j $NWORKERS
 }
 
 $ACTION
-#digi
-#meas2sp
