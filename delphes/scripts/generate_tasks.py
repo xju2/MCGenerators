fbf3ce56b9ecbb9a266913b26660858a8205e831
xju <xju@cern.ch>
2022-05-13 11:13:07 -0700

    with a bash script

--- delphes/scripts/generate_tasks.py
+++ delphes/scripts/generate_tasks.py
@@ -2,8 +2,15 @@
 """
 import os
 
+def write_jobs(jobs, fname):
+    with open(fname, 'w') as f:
+        f.write('\n'.join(jobs))
+        f.write('\n')
+
 def generate(njobs: int, py8cmnd: str, task_fname: str, shifter: bool,
-    exclude_ana: bool, minbias: str, delphes_card: str):
+    minbias: str, delphes_card: str,
+    no_analysis: bool = False, no_delphes: bool = False
+    ):
     basedir = os.path.abspath(os.getcwd())
 
     minbias = "MinBias.pileup"
@@ -31,10 +38,17 @@ def generate(njobs: int, py8cmnd: str, task_fname: str, shifter: bool,
         analysis = "shifter " + analysis
 
     cmd_list = ["ln -s {}".format(x) for x in config_files]
-    if exclude_ana:
-        cmd_list += [delphe]
+    
+    if no_delphes and not no_analysis:
+        cmd_list.append(analysis)
+    elif not no_delphes and no_analysis:
+        cmd_list.append(delphe)
+    elif no_delphes and no_analysis:
+        raise RuntimeError("no_delphes and no_analysis cannot be both True")
     else:
-        cmd_list += [delphe, analysis]
+        cmd_list.append(delphe)
+        cmd_list.append(analysis)
+
 
     command = " && ".join(cmd_list)
 
@@ -45,9 +59,17 @@ def generate(njobs: int, py8cmnd: str, task_fname: str, shifter: bool,
         outname = outname_base.format(ijob)
         jobs.append(command.format(outname=outname))
 
-    with open(task_fname, 'w') as f:
-        f.write('\n'.join(jobs))
-        f.write('\n')
+    write_jobs(jobs, task_fname)
+
+
+def generate_runs(njobs: int, py8cmnd: str, delphes_card: str, task_fname: str, *args, **kwargs):
+    exe = os.path.abspath("run.sh")
+    jobs = []
+    for ijob in range(njobs):
+        cmd = " ".join([exe, py8cmnd, delphes_card, "{:05}".format(ijob)])
+        jobs.append(cmd)
+
+    write_jobs(jobs, task_fname)
 
 if __name__ == '__main__':
     import argparse
@@ -58,7 +80,8 @@ if __name__ == '__main__':
     add_arg("delphes_card", help="delphes card")
     add_arg('--shifter', action='store_true', help='using shifter container')
     add_arg('-n', '--njobs', help='Number of jobs', default=100, type=int)
-    add_arg('-e', "--exclude-ana", action='store_true', help='exclude analysis')
     add_arg('-m', '--minbias', default='MinBias.pileup', help='MiniBias file')
+    add_arg('--no-delphes', action='store_true', help='do not run delphes')
+    add_arg('--no-analysis', action='store_true', help='do not run analysis')
     args = parser.parse_args()
-    generate(**vars(args))
+    generate_runs(**vars(args))
